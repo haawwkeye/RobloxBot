cc85f25bc22091dbd2e0375ae12603ac27e12a20
haawwkeye <connorbishop53@gmail.com>
2023-01-26 18:39:54 -0500

    Simple roblox follow bot!

---
+++ index.js
@@ -0,0 +1,68 @@
+require('dotenv').config();
+
+const noblox = require('noblox.js');
+const fs = require('fs');
+const path = require('path');
+
+const lastUserId = path.join(__dirname, 'lastUser.id');
+
+let currentUserId = 1;
+
+if (fs.existsSync(lastUserId)) {
+    currentUserId = Number(fs.readFileSync(lastUserId));
+    if (isNaN(currentUserId)) currentUserId = 1;
+    currentUserId = Math.floor(currentUserId);
+}
+
+// #region Death
+
+const DEATH = require('like-process');
+const { setTimeout } = require('timers/promises');
+
+let hasDied = false;
+let hasFileWrite = false;
+
+DEATH.on('cleanup', () => {
+	if (hasFileWrite) return;
+	hasFileWrite = true;
+
+	fs.writeFileSync(lastUserId, currentUserId.toString());
+
+	try {
+		process.exit(-1);
+	} catch (_) { /* empty */ }
+});
+// 'uncaughtException'
+DEATH.handle(['unhandledRejection', 'exit', 'SIGHUP', 'SIGINT', 'SIGTERM', 'SIGQUIT', 'SIGUSR1', 'SIGUSR2'], (evt, err) => {
+	if (hasDied) return;
+	hasDied = true;
+
+	// try {
+	// 	logger.info(`[DEATH] Event: ${evt} Error: ${err}`);
+	// } catch (_) {
+		console.log(`[DEATH] Event: ${evt} Error: ${err}`);
+	// }
+});
+// #endregion
+
+(async () => {
+    // Set the users cookie so we can "login" as them aka allow us to use .follow(UserID)
+    const currentUser = await noblox.setCookie(process.env.ROBLOSECURITY);
+    console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`);
+
+    setInterval(async () => {
+        console.log(`Attempting to follow ${currentUserId}`);
+        await noblox.follow(currentUserId).catch((res) => {
+            console.error(res);
+        }).then((res) => {
+            console.log(res);
+            currentUserId += 1;
+        });
+        // currentUserId += 1;
+    }, 5000);
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+        await setTimeout(1000, () => { /* empty */ });
+    }
+})();
